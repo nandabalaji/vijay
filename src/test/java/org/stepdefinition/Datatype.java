b8365243054113eb4bddd8c4ea3bd2d07556a0ba
karthikesan1999 <kesankarthi813@gmail.com>
2022-04-22 09:16:26 -0700

    task completed

---
+++ src/test/java/org/stepdefinition/Datatype.java
@@ -0,0 +1,15 @@
+package org.stepdefinition;
+
+public class Datatype {
+	public static void main(String[] args) {
+		int r=80;
+		String s="45";
+	int	j=Integer.parseInt(s);
+	System.out.println(r+j);
+	
+	boolean b=Boolean.parseBoolean(s);
+	System.out.println(b);
+	
+	}
+
+}
